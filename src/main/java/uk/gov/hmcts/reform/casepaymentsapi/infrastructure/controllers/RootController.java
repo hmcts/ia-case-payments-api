285b6591ca15139ffceb0583b9c6fa38560026eb
Ravi Aghera <Ravi.Aghera@HMCTS.net:wq>
2020-04-29 16:37:34 +0100

    Moving RootController

--- src/main/java/uk/gov/hmcts/reform/casepaymentsapi/infrastructure/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/reform/casepaymentsapi/infrastructure/controllers/RootController.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.casepaymentsapi.controllers;
+package uk.gov.hmcts.reform.casepaymentsapi.infrastructure.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
