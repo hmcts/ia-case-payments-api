d6ab0ba9aecc028e7a8933a400e12049c47e0dbb
tarun-palisetty <tarun.mca@gmail.com>
2020-05-19 14:06:11 +0100

    RIA-3139 unit tests added

--- src/main/java/uk/gov/hmcts/reform/iacasepaymentsapi/infrastructure/config/RestTemplateConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasepaymentsapi/infrastructure/config/RestTemplateConfiguration.java
@@ -18,7 +18,8 @@ public class RestTemplateConfiguration {
     @Bean
     public RestTemplate restTemplate(ObjectMapper objectMapper) {
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().removeIf(converter -> converter instanceof MappingJackson2HttpMessageConverter);
+        restTemplate.getMessageConverters()
+            .removeIf(converter -> converter instanceof MappingJackson2HttpMessageConverter);
         restTemplate.getMessageConverters().add(mappingJackson2HttpMessageConverter(objectMapper));
 
         return restTemplate;
