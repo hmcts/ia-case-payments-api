1d855144c08f2cdf2e570e9264344dd939f17e8c
Lee Philip <Lee365>
2020-07-21 14:38:02 +0100

    RIA-3412 pay and submit appeal

--- src/test/java/uk/gov/hmcts/reform/iacasepaymentsapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasepaymentsapi/domain/entities/ccd/EventTest.java
@@ -19,6 +19,6 @@ public class EventTest {
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
 
-        assertEquals(5, Event.values().length);
+        assertEquals(6, Event.values().length);
     }
 }
