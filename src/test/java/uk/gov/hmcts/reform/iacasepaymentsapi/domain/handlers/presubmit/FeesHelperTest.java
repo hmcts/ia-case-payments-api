1be0842c7f8542174e022e4952471d0123a46770
Brad Sorour <brad.sorour@gmail.com>
2022-01-25 21:16:39 +0000

    RIA-5119 unit test fix

--- src/test/java/uk/gov/hmcts/reform/iacasepaymentsapi/domain/handlers/presubmit/FeesHelperTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasepaymentsapi/domain/handlers/presubmit/FeesHelperTest.java
@@ -46,21 +46,6 @@ class FeesHelperTest {
     private final String feeAmountInPence =
         String.valueOf(new BigDecimal(fee.getAmountAsString()).multiply(new BigDecimal("100")));
 
-    @Test
-    void should_throw_exception_when_fee_is_null() {
-
-        when(asylumCase.read(DECISION_HEARING_FEE_OPTION, String.class)).thenReturn(Optional.of("decisionWithHearing"));
-        when(feeService.getFee(FeeType.FEE_WITH_HEARING)).thenReturn(null);
-
-        verify(asylumCase, times(0)).write(FEE_CODE, fee.getCode());
-        verify(asylumCase, times(0)).write(FEE_DESCRIPTION, fee.getDescription());
-        verify(asylumCase, times(0)).write(FEE_VERSION, fee.getVersion());
-        verify(asylumCase, times(0)).write(FEE_AMOUNT_GBP, fee.getAmountAsString());
-        verify(asylumCase, times(0)).write(FEE_PAYMENT_APPEAL_TYPE, YesOrNo.YES);
-
-        assertThatThrownBy(() -> FeesHelper.findFeeByHearingType(feeService, asylumCase))
-            .isExactlyInstanceOf(RestClientException.class);
-    }
 
     @Test
     void should_handle_fee_with_a_hearing() {
@@ -105,4 +90,60 @@ class FeesHelperTest {
         verify(asylumCase, times(0)).write(FEE_WITH_HEARING, fee.getAmountAsString());
         verify(asylumCase, times(1)).write(PAYMENT_DESCRIPTION, "Appeal determined without a hearing");
     }
+
+    @Test
+    void should_handle_fee_when_decision_hearing_fee_not_recognized() {
+
+        when(asylumCase.read(
+            DECISION_HEARING_FEE_OPTION,
+            String.class
+        )).thenReturn(Optional.of("unrecognized"));
+
+        when(feeService.getFee(FeeType.FEE_WITHOUT_HEARING)).thenReturn(fee);
+
+        FeesHelper.findFeeByHearingType(feeService, asylumCase);
+
+        verify(asylumCase, times(1)).write(FEE_CODE, fee.getCode());
+        verify(asylumCase, times(1)).write(FEE_DESCRIPTION, fee.getDescription());
+        verify(asylumCase, times(1)).write(FEE_VERSION, fee.getVersion());
+        verify(asylumCase, times(1)).write(FEE_AMOUNT_GBP, feeAmountInPence);
+        verify(asylumCase, times(1)).write(FEE_PAYMENT_APPEAL_TYPE, YesOrNo.YES);
+        verify(asylumCase, times(0)).write(FEE_WITHOUT_HEARING, fee.getAmountAsString());
+        verify(asylumCase, times(0)).write(FEE_WITH_HEARING, fee.getAmountAsString());
+        verify(asylumCase, times(0)).write(PAYMENT_DESCRIPTION, "Appeal determined with a hearing");
+        verify(asylumCase, times(0)).write(PAYMENT_DESCRIPTION, "Appeal determined without a hearing");
+    }
+
+    @Test
+    void should_throw_exception_when_fee_is_null() {
+
+        when(asylumCase.read(DECISION_HEARING_FEE_OPTION, String.class)).thenReturn(Optional.of("decisionWithHearing"));
+        when(feeService.getFee(FeeType.FEE_WITH_HEARING)).thenReturn(null);
+
+        verify(asylumCase, times(0)).write(FEE_CODE, fee.getCode());
+        verify(asylumCase, times(0)).write(FEE_DESCRIPTION, fee.getDescription());
+        verify(asylumCase, times(0)).write(FEE_VERSION, fee.getVersion());
+        verify(asylumCase, times(0)).write(FEE_AMOUNT_GBP, fee.getAmountAsString());
+        verify(asylumCase, times(0)).write(FEE_PAYMENT_APPEAL_TYPE, YesOrNo.YES);
+
+        assertThatThrownBy(() -> FeesHelper.findFeeByHearingType(feeService, asylumCase))
+            .isExactlyInstanceOf(RestClientException.class)
+            .hasMessageContaining("Cannot retrieve the fee from fees-register.");
+    }
+
+    @Test
+    void should_throw_exception_for_unrecognized_fee_type() {
+
+        when(asylumCase.read(
+            DECISION_HEARING_FEE_OPTION,
+            String.class
+        )).thenReturn(Optional.of("unrecognized"));
+
+        when(feeService.getFee(FeeType.FEE_WITH_HEARING)).thenReturn(fee);
+
+        assertThatThrownBy(() -> FeesHelper.findFeeByHearingType(feeService, asylumCase))
+            .isExactlyInstanceOf(RestClientException.class)
+            .hasMessageContaining("Cannot retrieve the fee from fees-register.");
+    }
+
 }
