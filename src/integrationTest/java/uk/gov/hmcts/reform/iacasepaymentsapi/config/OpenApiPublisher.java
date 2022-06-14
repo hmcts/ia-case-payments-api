141c6f4487fe5456261dd0ecc1e6a18f0ea79077
Hassan Sajjad <hassan.sajjad@version1.com>
2022-06-14 10:19:58 +0100

    Replace incompatible SpringFox lib for API documentation with SpringDoc

--- src/integrationTest/java/uk/gov/hmcts/reform/iacasepaymentsapi/config/OpenApiPublisher.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/iacasepaymentsapi/config/OpenApiPublisher.java
@@ -14,19 +14,19 @@ import uk.gov.hmcts.reform.iacasepaymentsapi.testutils.SpringBootIntegrationTest
  * Built-in feature which saves service's swagger specs in temporary directory.
  * Each travis run on master should automatically save and upload (if updated) documentation.
  */
-class SwaggerPublisher extends SpringBootIntegrationTest {
+class OpenApiPublisher extends SpringBootIntegrationTest {
 
     @DisplayName("Generate swagger documentation")
     @Test
     @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
     void generateDocs() throws Exception {
-        byte[] specs = mockMvc.perform(get("/v2/api-docs?group=ia-case-payments-api"))
+        byte[] specs = mockMvc.perform(get("/v3/api-docs?group=ia-case-payments-api"))
             .andExpect(status().isOk())
             .andReturn()
             .getResponse()
             .getContentAsByteArray();
 
-        try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/swagger-specs.json"))) {
+        try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/openapi-specs.json"))) {
             outputStream.write(specs);
         }
 
