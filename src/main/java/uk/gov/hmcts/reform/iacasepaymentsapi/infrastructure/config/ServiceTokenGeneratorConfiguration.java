83801c4a7d537c69ef48f18e6fda8126cb665227
rajeshthuraiyur <rajesh.thuraiyur@gmail.com>
2022-06-22 23:22:21 +0100

    Updates to fix build failures

--- src/main/java/uk/gov/hmcts/reform/iacasepaymentsapi/infrastructure/config/ServiceTokenGeneratorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasepaymentsapi/infrastructure/config/ServiceTokenGeneratorConfiguration.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.iacasepaymentsapi.infrastructure.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
 
 @Configuration
+@Slf4j
 public class ServiceTokenGeneratorConfiguration {
 
     public static final String SERVICE_AUTHORIZATION = "ServiceAuthorization";
@@ -18,6 +20,8 @@ public class ServiceTokenGeneratorConfiguration {
         @Value("${idam.s2s-auth.microservice}") String microService,
         ServiceAuthorisationApi serviceAuthorisationApi
     ) {
+        log.info("Secret " + secret);
+        log.info("microService " + microService);
         return AuthTokenGeneratorFactory.createDefaultGenerator(
             secret,
             microService,
