8b6ee6bae81117ca0e6255a6be2d5ace7f53a8ad
rajeshthuraiyur <rajesh.thuraiyur@gmail.com>
2022-06-23 12:44:55 +0100

    updates to remove logging

--- src/main/java/uk/gov/hmcts/reform/iacasepaymentsapi/infrastructure/config/ServiceTokenGeneratorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasepaymentsapi/infrastructure/config/ServiceTokenGeneratorConfiguration.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.iacasepaymentsapi.infrastructure.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +8,6 @@ import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
 
 @Configuration
-@Slf4j
 public class ServiceTokenGeneratorConfiguration {
 
     public static final String SERVICE_AUTHORIZATION = "ServiceAuthorization";
@@ -20,8 +18,6 @@ public class ServiceTokenGeneratorConfiguration {
         @Value("${idam.s2s-auth.microservice}") String microService,
         ServiceAuthorisationApi serviceAuthorisationApi
     ) {
-        log.info("Secret " + secret);
-        log.info("microService " + microService);
         return AuthTokenGeneratorFactory.createDefaultGenerator(
             secret,
             microService,
--- src/main/java/uk/gov/hmcts/reform/iacasepaymentsapi/infrastructure/security/oauth2/IdamSystemTokenGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/iacasepaymentsapi/infrastructure/security/oauth2/IdamSystemTokenGenerator.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.iacasepaymentsapi.infrastructure.security.oauth2;
 
 import feign.FeignException;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
@@ -11,7 +10,6 @@ import uk.gov.hmcts.reform.iacasepaymentsapi.infrastructure.clients.model.idam.T
 import uk.gov.hmcts.reform.iacasepaymentsapi.infrastructure.security.SystemTokenGenerator;
 
 @Component
-@Slf4j
 public class IdamSystemTokenGenerator implements SystemTokenGenerator {
 
     private final String systemUserName;
@@ -51,13 +49,6 @@ public class IdamSystemTokenGenerator implements SystemTokenGenerator {
         map.add("username", systemUserName);
         map.add("password", systemUserPass);
         map.add("scope", systemUserScope);
-
-        log.info("redirect_uri :" + idamRedirectUrl);
-        log.info("client_id :" + idamClientId);
-        log.info("client_secret :" + idamClientSecret);
-        log.info("username :" + systemUserName);
-        log.info("password :" + systemUserPass);
-        log.info("scope :" + systemUserScope);
 
         try {
 
