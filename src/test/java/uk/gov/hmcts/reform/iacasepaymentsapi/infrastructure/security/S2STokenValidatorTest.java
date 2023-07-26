6bce1288d083343cd0b78dd838e43f6f77c48b34
BurnsT <tim.burns@HMCTS.net>
2023-07-26 13:09:25 +0100

    RIA-6394 - test fixes

--- src/test/java/uk/gov/hmcts/reform/iacasepaymentsapi/infrastructure/security/S2STokenValidatorTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasepaymentsapi/infrastructure/security/S2STokenValidatorTest.java
@@ -11,12 +11,13 @@ import uk.gov.hmcts.reform.authorisation.validators.AuthTokenValidator;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class S2STokenValidatorTest {
 
-    private static final List<String> IA_S2S_AUTH_SERVICES = List.of("iac,payment_app");
+    private static final List<String> IA_S2S_AUTH_SERVICES = List.of("iac", "payment_app");
 
     @Mock
     private AuthTokenValidator authTokenValidator;
@@ -31,12 +32,26 @@ class S2STokenValidatorTest {
     @Test
     void givenServiceNameIsValid() {
         when(authTokenValidator.getServiceName("Bearer payment_app")).thenReturn("payment_app");
-        assertThrows(AccessDeniedException.class, () -> s2STokenValidator.checkIfServiceIsAllowed("payment_app"));
+        s2STokenValidator.checkIfServiceIsAllowed("payment_app");
+        verify(authTokenValidator).getServiceName("Bearer payment_app");
+    }
+
+    @Test
+    void givenServiceNameIsValidButWithNoBearerDefinition() {
+        when(authTokenValidator.getServiceName("Bearer payment_app")).thenReturn("payment_app");
+        s2STokenValidator.checkIfServiceIsAllowed("payment_app");
+        verify(authTokenValidator).getServiceName("Bearer payment_app");
     }
 
     @Test
     void givenServiceNameIsNullFromToken() {
         when(authTokenValidator.getServiceName("Bearer TestService")).thenReturn(null);
+        assertThrows(AccessDeniedException.class, () -> s2STokenValidator.checkIfServiceIsAllowed("TestService"));
+    }
+
+    @Test
+    void givenServiceNameCouldNotBeFound() {
+        when(authTokenValidator.getServiceName("Bearer TestService")).thenReturn("SERVICE_NAME_SHOULD_NOT_BE_FOUND");
         assertThrows(AccessDeniedException.class, () -> s2STokenValidator.checkIfServiceIsAllowed("TestService"));
     }
 
