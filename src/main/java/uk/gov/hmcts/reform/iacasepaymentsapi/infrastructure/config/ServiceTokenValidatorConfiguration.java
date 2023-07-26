d4559506ed97aa6e1ac6fe228962663d2ce84568
BurnsT <tim.burns@HMCTS.net>
2023-07-26 10:23:05 +0100

    RIA-6394 - added configuration class explicitly defining authtokenvalidation bean

--- src/main/java/uk/gov/hmcts/reform/iacasepaymentsapi/infrastructure/config/ServiceTokenValidatorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasepaymentsapi/infrastructure/config/ServiceTokenValidatorConfiguration.java
@@ -1,14 +1,9 @@
 package uk.gov.hmcts.reform.iacasepaymentsapi.infrastructure.config;
 
-import feign.Feign;
-import feign.jackson.JacksonEncoder;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
-import uk.gov.hmcts.reform.authorisation.generators.ServiceAuthTokenGenerator;
 import uk.gov.hmcts.reform.authorisation.validators.AuthTokenValidator;
 import uk.gov.hmcts.reform.authorisation.validators.ServiceAuthTokenValidator;
 
