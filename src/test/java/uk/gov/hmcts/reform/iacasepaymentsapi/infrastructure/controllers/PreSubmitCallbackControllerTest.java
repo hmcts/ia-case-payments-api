59cb1903a5aba6ce682542bb55c5ef2f6cea4cd5
Ivan Kirsanov <ivan.kirsanov@version1.com>
2023-11-29 09:53:36 +0100

    RIA-rebuild: rebuild payments

--- src/test/java/uk/gov/hmcts/reform/iacasepaymentsapi/infrastructure/controllers/PreSubmitCallbackControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasepaymentsapi/infrastructure/controllers/PreSubmitCallbackControllerTest.java
@@ -34,7 +34,6 @@ public class PreSubmitCallbackControllerTest {
 
     @BeforeEach
     public void setUp() {
-
         preSubmitCallbackController =
             new PreSubmitCallbackController(callbackDispatcher);
     }
