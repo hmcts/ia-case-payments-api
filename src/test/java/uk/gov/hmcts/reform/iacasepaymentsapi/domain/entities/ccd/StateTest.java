6c0a059ce5718d2182322084adeb4c4d54d1ec60
Aliveni Choppa <aliveni.choppa1@hmcts.net>
2024-08-22 10:24:08 +0100

    fix unit test

--- src/test/java/uk/gov/hmcts/reform/iacasepaymentsapi/domain/entities/ccd/StateTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasepaymentsapi/domain/entities/ccd/StateTest.java
@@ -19,6 +19,6 @@ class StateTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(5, State.values().length);
+        assertEquals(6, State.values().length);
     }
 }
