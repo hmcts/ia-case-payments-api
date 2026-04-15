a48f714b979d88778468a6ac160bcf90ec234cf6
nilay <nilay.patel@solirius.com>
2026-04-15 14:59:59 +0100

    further improvement

--- src/functionalTest/java/uk/gov/hmcts/reform/iacasepaymentsapi/CcdScenarioRunnerTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacasepaymentsapi/CcdScenarioRunnerTest.java
@@ -164,8 +164,7 @@ public class CcdScenarioRunnerTest {
                                                      long testCaseId,
                                                      Map<String, Object> expectedResponse) throws IOException {
         int maxRetries = 3;
-        assumeFalse(description.startsWith("Disabled:"),
-                    "Test skipped because description starts with 'Disabled:'");
+        assumeFalse(description.startsWith("Disabled:"), "Test marked as disabled");
         for (int i = 0; i < maxRetries; i++) {
             try {
                 actualResponse = null;
