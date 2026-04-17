abae31772c0348be2eaf1b4970282068418e7f5e
nilay <nilay.patel@solirius.com>
2026-04-17 09:24:40 +0100

    event test improvement

--- src/test/java/uk/gov/hmcts/reform/iacasepaymentsapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasepaymentsapi/domain/entities/ccd/EventTest.java
@@ -1,29 +1,44 @@
 package uk.gov.hmcts.reform.iacasepaymentsapi.domain.entities.ccd;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class EventTest {
-
-    @Test
-    void has_correct_values() {
-
-        assertEquals("startAppeal", Event.START_APPEAL.toString());
-        assertEquals("editAppeal", Event.EDIT_APPEAL.toString());
-        assertEquals("paymentAppeal", Event.PAYMENT_APPEAL.toString());
-        assertEquals("payAndSubmitAppeal", Event.PAY_AND_SUBMIT_APPEAL.toString());
-        assertEquals("payForAppeal", Event.PAY_FOR_APPEAL.toString());
-        assertEquals("submitAppeal", Event.SUBMIT_APPEAL.toString());
-        assertEquals("recordRemissionDecision", Event.RECORD_REMISSION_DECISION.toString());
-        assertEquals("updatePaymentStatus", Event.UPDATE_PAYMENT_STATUS.toString());
-        assertEquals("generateServiceRequest", Event.GENERATE_SERVICE_REQUEST.toString());
-        assertEquals("unknown", Event.UNKNOWN.toString());
+    @ParameterizedTest
+    @MethodSource("eventMapping")
+    void has_correct_values(String expected, String actual) {
+        assertEquals(expected, actual);
     }
 
     @Test
-    void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
+    void if_this_test_fails_it_is_because_eventMapping_needs_updating_with_your_changes() {
+        List<String> eventMappingStrings = eventMapping().map(arg -> arg.get()[1])
+            .map(String.class::cast)
+            .toList();
+        List<Event> missingEvents = Arrays.stream(Event.values())
+            .filter(event -> !eventMappingStrings.contains(event.toString())).toList();
+        assertTrue(missingEvents.isEmpty(), "The following events are missing from the eventMapping method: " + missingEvents);
+    }
 
-        assertEquals(10, Event.values().length);
+    static Stream<Arguments> eventMapping() {
+        return Stream.of(
+            Arguments.of("startAppeal", Event.START_APPEAL.toString()),
+            Arguments.of("editAppeal", Event.EDIT_APPEAL.toString()),
+            Arguments.of("paymentAppeal", Event.PAYMENT_APPEAL.toString()),
+            Arguments.of("submitAppeal", Event.SUBMIT_APPEAL.toString()),
+            Arguments.of("payAndSubmitAppeal", Event.PAY_AND_SUBMIT_APPEAL.toString()),
+            Arguments.of("payForAppeal", Event.PAY_FOR_APPEAL.toString()),
+            Arguments.of("recordRemissionDecision", Event.RECORD_REMISSION_DECISION.toString()),
+            Arguments.of("updatePaymentStatus", Event.UPDATE_PAYMENT_STATUS.toString()),
+            Arguments.of("generateServiceRequest", Event.GENERATE_SERVICE_REQUEST.toString())
+        );
     }
 }
